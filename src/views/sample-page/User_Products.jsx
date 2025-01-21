7ac3731c9d8ea85767bf4bc2bef9ff965ab21d95
ChaimaGhanney <chaimaghanney05@gmail.com>
2025-01-21 08:54:57 +0100

    test1

--- src/views/sample-page/User_Products.jsx
+++ src/views/sample-page/User_Products.jsx
@@ -318,7 +318,7 @@ const AllProducts = () => {
                         <b>Contract type:</b> {product.categorie_produit}
                       </Typography>
                       <Typography variant="body2" color="textSecondary" sx={{ marginBottom: 1 }}>
-                        <b>Status:</b> {product.etat === 'hors_stock' ? 'Present' : 'Absent'}
+                        <b>Status:</b> {product.etat}
                       </Typography>
                       <Typography variant="body2" color="textSecondary" sx={{ marginBottom: 1 }}>
                         <b>Job:</b> {product.Marque}
